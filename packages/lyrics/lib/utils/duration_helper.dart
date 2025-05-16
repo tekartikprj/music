b2cdc85ad89129e3454baa941967b114325ecdd8
Alexandre Roux <alex@tekartik.com>
2025-05-16 19:09:57 +0200

    export lrcFormatDuration

--- packages/lyrics/lib/utils/duration_helper.dart
+++ packages/lyrics/lib/utils/duration_helper.dart
@@ -1,4 +1,11 @@
+import 'package:tekaly_lyrics/src/lyrics.dart';
+
 export 'package:tekaly_lyrics/src/duration_helper.dart'
     show TekartikLyricsIntExt;
 
 export 'package:tekartik_common_utils/duration_utils.dart';
+
+/// Formats a duration to a string in the LRC format
+String lrcFormatDuration(Duration duration) {
+  return formatLyricsDuration(duration);
+}
